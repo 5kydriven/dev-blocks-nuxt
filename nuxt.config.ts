c693cb0b493568914002e0b02acb7b9425daa0a6
5kydriven <tagpuno619@gmail.com>
2025-12-25 11:53:03 +0800

    feat: add nuxt content and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
-	modules: ['@nuxt/ui'],
+	modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/eslint'],
+	eslint: {
+		config: {
+			stylistic: true,
+		},
+	},
 });
